60da7cd716864380dcf44d37e9cf73c0808759bb
perquantum <perquantum@gmail.com>
2022-02-26 17:32:17 +0530

    item-cart-system edit

--- my-first-app/src/app/product-description/product-description.component.ts
+++ my-first-app/src/app/product-description/product-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-description',
@@ -11,14 +11,19 @@ export class ProductDescriptionComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  add = 0;
-    @Output() newItemEvent = new EventEmitter<string>();
-    addNewItem(value: string) {
-      this.newItemEvent.emit(value);
-    }
 
-  addToCart(){
-    
+  count = 0;
+ 
+  @Input() ClickCounter = '';
+  @Output() countChanged: EventEmitter<number> =   new EventEmitter();
+
+  increment() {
+      this.count++;
+      this.countChanged.emit(this.count);
+    }
+  decrement() {
+      this.count--;
+      this.countChanged.emit(this.count);
   }
 
 }
--- my-first-app/src/app/product/product.component.ts
+++ my-first-app/src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -12,13 +12,77 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  desc = false;
-  show_description(){
-    if(this.desc==false){
-      this.desc = true;
+  // desc = false;
+  // show_description(){
+  //   if(this.desc==false){
+  //     this.desc = true;
+  //   }
+  //   else{
+  //     this.desc=false;
+  //   }
+  // }
+  // ClickCounter = 0;
+ 
+  // countChangedHandler(count: number) {
+  //   this.ClickCounter = count;
+  //   console.log(count);
+  // }
+
+  products = [
+    {
+      "id" : 101,
+      "name" : "item1",
+      "price" : 100,
+      "quantity" : 0,
+      "loc" : "Baroda"
+    },
+    {
+      "id" : 102,
+      "name" : "item2",
+      "price" : 300,
+      "quantity" : 0,
+      "loc" : "Amreli"
+    },
+    {
+      "id" : 103,
+      "name" : "item3",
+      "price" : 250,
+      "quantity" : 0,
+      "loc" : "Ahmedabad"
+    },
+    {
+      "id" : 104,
+      "name" : "item4",
+      "price" : 750,
+      "quantity" : 0,
+      "loc" : "Mumbai"
     }
-    else{
-      this.desc=false;
-    }
+  ]
+
+  quantity = 0;
+  value = 0;
+  tableIndex = 0;
+  itemQuantity = 0;
+  amount = 0;
+  deatils:any = [];
+  @Output() newItemEvent = new EventEmitter<any>();
+  addItem(name: string, price: number, quantity: number ) {
+    this.deatils = [name, price, quantity]
+    this.amount = this.deatils.price*this.deatils.quantity;
+    this.newItemEvent.emit(this.deatils);
   }
+  decrement(value:number, index:number){
+    value--;
+    if(value<0){
+      value=0;
+    }
+    let i = this.products.findIndex(x => x.id === index)
+    this.products[i].quantity = value;
+  }
+  increment(value:number, index:number){
+    value++;
+    let i = this.products.findIndex(x => x.id === index)
+    this.products[i].quantity = value;
+  }
+
 }
